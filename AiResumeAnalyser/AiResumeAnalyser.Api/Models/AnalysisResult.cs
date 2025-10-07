e0f5f66647dc2bca865161f9181fabd87f75511e
ThomasLy1 <Thomas.Ly1@fdmgroup.com>
2025-10-07 12:49:00 +0100

    Adding logic to model, with additional comments for clarity

--- AiResumeAnalyser/AiResumeAnalyser.Api/Models/AnalysisResult.cs
+++ AiResumeAnalyser/AiResumeAnalyser.Api/Models/AnalysisResult.cs
@@ -1,6 +1,15 @@
 ﻿namespace AiResumeAnalyser.Api.Models
 {
+    // Represents output from API
+    // ResumeAnalysisService sends the prompt to OpenAPI and receives JSON response
+    // JSON is deserialized into an AnalysisResult object
+    // Blazor frontend will read this object and display it to the user.
+    // Acts as an answer received from the AI model (IN JSON FORMAT)
     public class AnalysisResult
     {
+        public int MatchScore { get; set; }
+        public List<string>? MissingKeywords { get; set; }
+        public List<string>? Strengths { get; set; }
+        public List<string>? Suggestions { get; set; }
     }
 }
---
+++ AiResumeAnalyser/AiResumeAnalyser.Api/Models/ResumeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AiResumeAnalyser.Api.Models
+{
+    // record is lightweight, immutable data class in C#
+    // Represents the input data that the API receives from the frontend
+    // Serves as input to the ResumeAnalyzerService
+    // Acts as a question sent to the AI model
+    public record ResumeRequest(string ResumeText, string JobDescription);
+}
